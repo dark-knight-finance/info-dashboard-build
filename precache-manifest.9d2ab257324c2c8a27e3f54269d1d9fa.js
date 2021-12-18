cd6aef4605d2aa96f1af1432d69e335bfbb3f16f
Shahzeb Khan <shahzeb8285@gmail.com>
2021-12-18 07:48:10 +0530

    first commit

---
+++ precache-manifest.9d2ab257324c2c8a27e3f54269d1d9fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8245d26c04f8d544c46734a75b1f9cf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46de2c67f174da06ead6",
+    "url": "/static/js/2.afb13934.chunk.js"
+  },
+  {
+    "revision": "8efadbe79de939f183de4c88d2491df0",
+    "url": "/static/js/2.afb13934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b0aa446e393e90fd92",
+    "url": "/static/js/main.88ffb66f.chunk.js"
+  },
+  {
+    "revision": "551c9e280db88d76f240",
+    "url": "/static/js/runtime-main.c307dd36.js"
+  },
+  {
+    "revision": "66ad3242972fe260304197d77cf77225",
+    "url": "/static/media/logo.66ad3242.svg"
+  }
+]);
